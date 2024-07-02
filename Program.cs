96c8dbe8fe35fadbc9136927d59c4462c0245c09
ThinkPad T14 <ThinkPad T14@DESKTOP-C9FJOHD>
2024-07-02 13:53:11 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> Siswa = new List<string>()
+        {
+            "Rifki","Yoga","Lathif","Dhafa","Agung"
+        };
+
+        //Menampilkan Semua
+        Console.WriteLine(string.Join("\n",Siswa));
+    }
+}
+
